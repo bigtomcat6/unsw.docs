2f8880e43ceecddcc15dd491bd9ef05c57344d6a
bigtomcat6 <bigtomcat@qq.com>
2024-06-09 15:47:17 +1000

    Added component: HLink

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -3,10 +3,12 @@ import './styles/Style.scss'
 import { defineClientConfig } from 'vuepress/client'
 
 import HButton from './components/HButton.vue'
+import HLink from './components/HLink.vue'
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
-    app.component('HButton', HButton)
+    app.component('HButton', HButton);
+    app.component('HLink', HLink);
   },
   setup() {},
   rootComponents: [],
---
+++ docs/.vuepress/components/HLink.vue
@@ -0,0 +1,63 @@
+<template>
+  <!--https://iconify.design/docs/icon-components/vue/-->
+  <a :href="src" target="_blank" style="display: inline-flex;">
+    <Icon :icon="autoIcon" style="margin-right: 7px; font-size: 18px;" />
+    <text>{{ title }}</text>
+  </a>
+</template>
+
+
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
+<script>
+export default {
+  props: {
+    /**
+     * @type {'link' | 'pdf'}
+     * @default 'link'
+     */
+    type: {
+      type: String,
+      default: 'Link'
+    },
+    title: {
+      type: String,
+      default: 'Link'
+    },
+    src: {
+      type: String,
+      default: undefined
+    }
+  },
+  computed: {
+    autoIcon() {
+      switch (this.type) {
+        case 'pdf':
+          return 'grommet-icons:document-pdf'
+          break;
+        
+        case 'link':
+        default:
+          return 'lucide:link'
+          break;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+a {
+    font-weight: 500;
+    color: var(--vp-c-brand-1);
+    text-decoration: underline;
+    text-underline-offset: 2px;
+    transition: color var(--t-color), opacity var(--t-color), text-underline-offset var(--t-color);
+}
+a:hover {
+    color: var(--vp-c-brand-2);
+    text-underline-offset: 5px;
+}
+</style>
