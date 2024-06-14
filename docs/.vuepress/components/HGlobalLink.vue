c01e22c0116e8bcd1ad605a42b736204ff1d1f77
bigtomcat6 <bigtomcat@qq.com>
2024-06-14 15:56:39 +1000

    测试HGlobalLink

---
+++ docs/.vuepress/components/HGlobalLink.vue
@@ -0,0 +1,27 @@
+<template>
+
+123
+
+
+</template>
+
+<script setup>
+import axios from 'axios';
+import yaml from 'js-yaml';
+
+try {
+  const file = await axios.get('/HGlobalLink.yaml');
+  const data = yaml.load(file.data);
+  console.log(data);
+} catch (e) {
+  console.log(e);
+}
+</script>
+
+<script>
+
+export default {
+  
+}
+
+</script>
