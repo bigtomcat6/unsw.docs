99c67c64019f1f68b1fa1778f7f32cd08c197ae2
bigtomcat6 <bigtomcat@qq.com>
2024-07-01 16:54:06 +1000

    Add HSelect

---
+++ docs/.vuepress/components/HSelect.vue
@@ -0,0 +1,38 @@
+<template>
+  <select>
+    <option v-for="value in values" :value="value.label" :selected="value.selected | false">{{ value.label }}</option>
+  </select>
+</template>
+
+<script>
+
+export default {
+  props: {
+    /**
+     * @type {'Mobius'}
+     * @default 'Mobius'
+     */
+    type: {
+      type: String,
+      default: 'Mobius'
+    },
+
+    /**
+     * @type [{
+     *  label: string,
+     *  selected?: boolean
+     * }]
+     */
+    values: {
+      type: Array,
+      default: [
+        {label: 'A'},
+      ]
+    }
+  },
+  computed: {
+
+  }
+}
+
+</script>
