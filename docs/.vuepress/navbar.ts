df9f143f299e771e0256b8868b61bf190ac78717
bigtomcat6 <bigtomcat@qq.com>
2024-09-05 12:38:45 +1000

    refactor: separate sidebar & navbar

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,22 @@
+import { NavItem } from 'vuepress-theme-plume'
+
+export const navbar: NavItem[] = [
+  {
+    link: '/',
+    text: 'Home',//`${Text()}`,
+    icon: 'ri:home-8-line'
+  },
+  {
+    text: 'Current',  icon: 'mdi:university-outline',
+    items: [
+      { text: 'COMP9444', link: '/COMP9444/' },
+    ]
+  },
+  { text: 'Links', link: '/useful/', icon: 'fa6-solid:link' },
+  { text: 'Help', icon: 'mi:circle-help',
+    items: [
+      { text: 'About', link: '/help/about' },
+      { text: 'Copyright', link: '/help/copyright' }
+    ]
+  },
+];
